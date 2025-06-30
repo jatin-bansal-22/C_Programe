50832abbea2fe480dd3e53713d97711b0ee54e72
jatin-bansal-22 <jatinb1901@gmail.com>
2025-06-30 17:23:48 +0530

    Add files via upload

---
+++ zcc2.c
@@ -0,0 +1,10 @@
+// program to read and write your name using gets and puts
+#include<stdio.h>
+void main()
+{ 
+    char name[20];
+    printf("\nplease enter the name");
+    gets(name);
+    printf("hello");
+    puts(name);
+}
