377d0e7ad55cc60f99c52aa6ebff1b7a26a12e37
jatin-bansal-22 <jatinb1901@gmail.com>
2025-06-30 16:46:52 +0530

    Add files via upload

---
+++ ab_sum.c
@@ -0,0 +1,12 @@
+// sum of two numbers
+#include <stdio.h>
+int main()
+{
+    int a, b, sum;
+    a = 40;
+    b = 60;
+    sum = a + b;
+    printf("thus the sum of a and b is = % d", sum);
+    return 0;
+}
+a
